80671e181d27747cebd5cfdba913a52bcb212f9f
DanielFariias <danielfariias15@gmail.com>
2024-01-24 21:59:12 -0300

    Add createAndAuthenticateUser test utility function

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  const createResponse = await request(app.server).post('/users').send({
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: '12345678',
+  })
+
+  const authResponse = await request(app.server).post('/sessions').send({
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: '12345678',
+  })
+
+  const { token } = authResponse.body
+
+  return { token }
+}
